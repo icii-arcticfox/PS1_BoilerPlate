a34ccf142c80820832575327eebed56bee8552d1
SuperMB <littlebass09@tamu.edu>
2023-10-18 00:02:46 -0700

    MB Fixed bug

--- Automations/Vars.py
+++ Automations/Vars.py
@@ -4,10 +4,6 @@ from Icii import *
 class Vars(PythonAutomation): 
     def ApplyAutomation(self):
 
-        with self.CodeAfterAutomation :
-            print("Howdy!")
-
-
-        # for item in self.Items: 
-        #     with self.CodeAfterAutomation :
-        #         ((item)) = if_contains(dictionary, '((item))')
+        for item in self.Items: 
+            with self.CodeAfterAutomation :
+                ((item)) = if_contains(dictionary, '((item))')
